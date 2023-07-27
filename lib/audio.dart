019bab72e4dc9809533a55829655c43683f39b94
nirokishan <nirokishan>
2023-07-27 22:47:13 +0530

    first commit

--- lib/audio.dart
+++ lib/audio.dart
@@ -1,6 +1,7 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_file_downloader/flutter_file_downloader.dart';
+import 'package:permission_handler/permission_handler.dart';
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -9,51 +10,58 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
   late AnimationController iconController;
   bool isAnimated = false;
-  bool shopPause = false;
   double _volume = 1.0;
-  bool isPlaying =false;
-  Duration _duration= Duration.zero;
-  Duration _position=Duration.zero;
+  bool isPlaying = false;
+  Duration _duration = Duration.zero;
+  Duration _position = Duration.zero;
+  double? _progress;
   final player = AudioPlayer();
   String audioUrl =
-   'https://media.radioinstore.in/songs/Tamil/1987/rettai%20val%20kuruvi/raja%20raja%20cholan_kj%20jesudoss.mp3';
+      'https://media.radioinstore.in/songs/Tamil/1987/rettai%20val%20kuruvi/raja%20raja%20cholan_kj%20jesudoss.mp3';
+  String filename = "audio.mp3";
+
   @override
-void initState() {
-  super.initState();
-  player.onPlayerStateChanged.listen((state) {
-    setState(() {
-      isPlaying = state == PlayerState.playing;
+  void initState() {
+    super.initState();
+    player.onPlayerStateChanged.listen((state) {
+      setState(() {
+        isPlaying = state == PlayerState.playing;
+      });
     });
-  });
-  player.onDurationChanged.listen((newDuration) {
-    setState(() {
-      _duration = newDuration;
+    player.onDurationChanged.listen((newDuration) {
+      setState(() {
+        _duration = newDuration;
+      });
     });
-  });
-  player.onPositionChanged.listen((newPosition) {
-    setState(() {
-      _position = newPosition;
+    player.onPositionChanged.listen((newPosition) {
+      setState(() {
+        _position = newPosition;
+      });
     });
-  });
 
-  iconController = AnimationController(
-    vsync: this,
-    duration: Duration(milliseconds: 1000),
-  );
+    iconController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
 
-  playAudioFromURL(audioUrl);
-}
-
-
-  void playAudioFromURL(String audioUrl) async {
+    playAudioFromURL(audioUrl);
+    requestPermissions();
+  }
+void playAudioFromURL(String audioUrl) async {
     await player.setSourceUrl(audioUrl);
     player.resume();
   }
+  void requestPermissions() async {
+  Map<Permission, PermissionStatus> statuses = await [
+    Permission.storage,
+  ].request();
 
-  
-String formatTime(int seconds){
-  return '${(Duration(seconds: seconds))}'.split(".")[0].padLeft(8,'0');
 }
+
+  String formatTime(int seconds) {
+    return '${Duration(seconds: seconds)}'.split(".")[0].padLeft(8, '0');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,47 +70,41 @@ String formatTime(int seconds){
         title: Text("Audio player"),
         backgroundColor: Colors.lightGreen,
       ),
-      body: Center(
+      body:
+
+      SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            SizedBox(height: 100,),
             Slider(
-              min:0 ,
-            max:_duration.inSeconds.toDouble(),
-            value: _position.inSeconds.toDouble(),
-             onChanged: (value){
-            _position=Duration(seconds: value.toInt());
-              player.seek(_position);
-              player.resume();
-             }
-             ),
-             Container(
+              min: 0,
+              max: _duration.inSeconds.toDouble(),
+              value: _position.inSeconds.toDouble(),
+              onChanged: (value) {
+                _position = Duration(seconds: value.toInt());
+                player.seek(_position);
+                player.resume();
+              },
+            ),
+            Container(
               padding: const EdgeInsets.all(20),
-             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                 Text(formatTime(_position.inSeconds)),
-                Text(formatTime((_duration -_position).inSeconds)),
-              ],
-               ),
-             ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(formatTime(_position.inSeconds)),
+                  Text(formatTime((_duration - _position).inSeconds)),
+                ],
+              ),
+            ),
             SizedBox(height: 30),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 SizedBox(width: 15),
-                //backward button
-             /*IconButton(
-                  icon: Icon(CupertinoIcons.backward_fill),
-                  onPressed: () {
-              //      player.seek(Duration(seconds: player.duration.inSeconds - 30));
-                  },
-                ),*/
-                SizedBox(width: 15),
-                //play pause button
                 GestureDetector(
                   onTap: () {
-                    AnimateIcon();
+                    animateIcon();
                   },
                   child: AnimatedIcon(
                     icon: AnimatedIcons.play_pause,
@@ -111,14 +113,6 @@ String formatTime(int seconds){
                     color: Colors.red,
                   ),
                 ),
-                SizedBox(width: 15),
-                //forward button
-         /*       IconButton(
-                  icon: Icon(CupertinoIcons.forward_fill),
-                  onPressed: () {
-                //    player.seek(Duration(seconds: player.duration.inSeconds + 30));
-                  },
-                ),*/
                 SizedBox(width: 15),
               ],
             ),
@@ -130,29 +124,63 @@ String formatTime(int seconds){
                   onPressed: () {},
                   icon: Icon(Icons.volume_down_outlined),
                 ),
-               Slider(
-                    value: _volume,
-                    onChanged: (newValue) {
-                      setState(() {
-                        _volume = newValue;
-                      });
-                      player.setVolume(_volume);
-                    },
-                    min: 0.0,
-                    max: 1.0,
-                    activeColor: Colors.lightGreen,
-                    inactiveColor: Colors.grey,
-                  ),
+                Slider(
+                  value: _volume,
+                  onChanged: (newValue) {
+                    setState(() {
+                      _volume = newValue;
+                    });
+                    player.setVolume(_volume);
+                  },
+                  min: 0.0,
+                  max: 1.0,
+                  activeColor: Colors.lightGreen,
+                  inactiveColor: Colors.grey,
+                ),
               ],
+            ),
+            Row(
+             
+              children:[
+                 SizedBox(width: 60),
+                 
+            Text("Download click button"),
+           _progress != null ? CircularProgressIndicator() :
+            IconButton(
+              onPressed: ()  {
+            downloadAudioFile();
+              },
+              icon: Icon(Icons.download),
+            ),
+              ]
             ),
           ],
         ),
       ),
     );
   }
-
- //animation icon for play/pause
-  void AnimateIcon() {
+void downloadAudioFile() {
+  FileDownloader.downloadFile(
+    url: audioUrl.toString().trim(),
+    onProgress: (name, progress) {
+      setState(() {
+        _progress = progress;
+      });
+    },
+  ).then((value) {
+    
+    print("Download completed.");
+    setState(() {
+      _progress = null;
+    });
+  }).catchError((error) {
+    print("Download failed: $error");
+    setState(() {
+      _progress = null;
+    });
+  });
+}
+  void animateIcon() {
     setState(() {
       isAnimated = !isAnimated;
 
@@ -166,11 +194,10 @@ String formatTime(int seconds){
     });
   }
 
-@override
-void dispose() {
-  iconController.dispose();
-  player.dispose();
-  super.dispose();
-}
-
+  @override
+  void dispose() {
+    iconController.dispose();
+    player.dispose();
+    super.dispose();
+  }
 }
